591480e973ac901cb8e5fe525478f313d47376c5
unknown <you@example.com>
2020-03-17 16:37:38 +0530

    Jchanged

--- identity-system/backend/routes/Credential_EntityR.js
+++ identity-system/backend/routes/Credential_EntityR.js
@@ -1,12 +1,12 @@
-const Credential_Entity = require('../models/Credential_Entity.model')
-var Credential_EntityR = require('express').Router();
+const User = require('../models/Credential_Entity.model')
+var UserR= require('express').Router();
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
-Credential_EntityR.use(cors())
+UserR.use(cors())
 
-Credential_EntityR.post('/register', (req, res) => {
+UserR.post('/register', (req, res) => {
     const CredentialData = {
         email: req.body.email,
         password: req.body.password,
@@ -15,14 +15,14 @@ Credential_EntityR.post('/register', (req, res) => {
         API_key: req.body.API_key
     }
 
-    Credential_Entity.findOne({
+    User.findOne({
         email: req.body.email
     })
         .then(user => {
             if (!user) {
                 bcrypt.hash(req.body.password, 10, (err, hash) => {
                     CredentialData.password = hash
-                    Credential_Entity.create(CredentialData)
+                    User.create(CredentialData)
                         .then(user => {
                             res.json({ status: user.email + '  Registered!' })
                         })
@@ -40,8 +40,8 @@ Credential_EntityR.post('/register', (req, res) => {
 })
 
 
-Credential_EntityR.post('/login', (req, res) => {
-    Credential_Entity.findOne({
+UserR.post('/login', (req, res) => {
+    User.findOne({
         email: req.body.email
     })
         .then(user => {
@@ -50,7 +50,6 @@ Credential_EntityR.post('/login', (req, res) => {
                     // Passwords match
                     const payload = {
                         _id: user._id,
-                        
                         email: user.email,
                         owner: user.owner,
                         usage_plan: user.usage_plan,
@@ -73,15 +72,15 @@ Credential_EntityR.post('/login', (req, res) => {
         })
 })
 
-Credential_EntityR.get('/profile', (req, res) => {
+UserR.get('/profile', (req, res) => {
     var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
 
-    Credential_Entity.findOne({
+    User.findOne({
         _id: decoded._id
     })
-        .then(Credential => {
-            if (Credential) {
-                res.json(Credential)
+        .then(User => {
+            if (User) {
+                res.json(User)
             } else {
                 res.send('User does not exist')
             }
@@ -92,56 +91,56 @@ Credential_EntityR.get('/profile', (req, res) => {
 })
 
 
-// // users.post('/reset-password', function (req, res) {
-// //     const email = req.body.email
-// //     User
-// //         .findOne({
-// //             where: {email: email},//checking if the email address sent by client is present in the db(valid)
-// //         })
-// //         .then(function (user) {
-// //             if (!user) {
-// //                 return throwFailed(res, 'No user found with that email address.')
-// //             }
-// //             ResetPassword
-// //                 .findOne({
-// //                     where: {userId: user.id, status: 0},
-// //                 }).then(function (resetPassword) {
-// //                 if (resetPassword)
-// //                     resetPassword.destroy({
-// //                         where: {
-// //                             id: resetPassword.id
-// //                         }
-// //                     })
-// //                 token = crypto.randomBytes(32).toString('hex')//creating the token to be sent to the forgot password form (react)
-// //                 bcrypt.hash(token, null, null, function (err, hash) {//hashing the password to store in the db node.js
-// //                     ResetPassword.create({
-// //                         userId: user.id,
-// //                         resetPasswordToken: hash,
-// //                         expire: moment.utc().add(config.tokenExpiry, 'seconds'),
-// //                     }).then(function (item) {
-// //                         if (!item)
-// //                             return throwFailed(res, 'Oops problem in creating new password record')
-// //                         let mailOptions = {
-// //                             from: '"<jyothi pitta>" jyothi.pitta@ktree.us',
-// //                             to: user.email,
-// //                             subject: 'Reset your account password',
-// //                             html: '<h4><b>Reset Password</b></h4>' +
-// //                             '<p>To reset your password, complete this form:</p>' +
-// //                             '<a href=' + config.clientUrl + 'reset/' + user.id + '/' + token + '">' + config.clientUrl + 'reset/' + user.id + '/' + token + '</a>' +
-// //                             '<br><br>' +
-// //                             '<p>--Team</p>'
-// //                         }
-// //                         let mailSent = sendMail(mailOptions)//sending mail to the user where he can reset password.User id and the token generated are sent as params in a link
-// //                         if (mailSent) {
-// //                             return res.json({success: true, message: 'Check your mail to reset your password.'})
-// //                         } else {
-// //                             return throwFailed(error, 'Unable to send email.');
-// //                         }
-// //                     })
-// //                 })
-// //             });
-// //         })
-// //  })
+// users.post('/reset-password', function (req, res) {
+//     const email = req.body.email
+//     User
+//         .findOne({
+//             where: {email: email},//checking if the email address sent by client is present in the db(valid)
+//         })
+//         .then(function (user) {
+//             if (!user) {
+//                 return throwFailed(res, 'No user found with that email address.')
+//             }
+//             ResetPassword
+//                 .findOne({
+//                     where: {userId: user.id, status: 0},
+//                 }).then(function (resetPassword) {
+//                 if (resetPassword)
+//                     resetPassword.destroy({
+//                         where: {
+//                             id: resetPassword.id
+//                         }
+//                     })
+//                 token = crypto.randomBytes(32).toString('hex')//creating the token to be sent to the forgot password form (react)
+//                 bcrypt.hash(token, null, null, function (err, hash) {//hashing the password to store in the db node.js
+//                     ResetPassword.create({
+//                         userId: user.id,
+//                         resetPasswordToken: hash,
+//                         expire: moment.utc().add(config.tokenExpiry, 'seconds'),
+//                     }).then(function (item) {
+//                         if (!item)
+//                             return throwFailed(res, 'Oops problem in creating new password record')
+//                         let mailOptions = {
+//                             from: '"<jyothi pitta>" jyothi.pitta@ktree.us',
+//                             to: user.email,
+//                             subject: 'Reset your account password',
+//                             html: '<h4><b>Reset Password</b></h4>' +
+//                             '<p>To reset your password, complete this form:</p>' +
+//                             '<a href=' + config.clientUrl + 'reset/' + user.id + '/' + token + '">' + config.clientUrl + 'reset/' + user.id + '/' + token + '</a>' +
+//                             '<br><br>' +
+//                             '<p>--Team</p>'
+//                         }
+//                         let mailSent = sendMail(mailOptions)//sending mail to the user where he can reset password.User id and the token generated are sent as params in a link
+//                         if (mailSent) {
+//                             return res.json({success: true, message: 'Check your mail to reset your password.'})
+//                         } else {
+//                             return throwFailed(error, 'Unable to send email.');
+//                         }
+//                     })
+//                 })
+//             });
+//         })
+//  })
  
 
-module.exports = Credential_EntityR;
+module.exports = UserR;
